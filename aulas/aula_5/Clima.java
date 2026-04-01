3149e2a801576bc7266dc35b1d8168ef6735c3f3
bassottolucas <lucasbassottos@gmail.com>
2026-04-01 08:27:12 -0300

    Add files via upload

---
+++ aulas/aula_5/Clima.java
@@ -0,0 +1,21 @@
+package Estrutura_de_dados.Trabalho1;
+
+public class Clima {
+    public String ano;
+    public String mes;
+    public String temperatura;
+    public String precipitacao;
+
+    public Clima(String ano, String mes, String temperatura, String precipitacao) {
+        this.ano = ano;
+        this.mes = mes;
+        this.temperatura = temperatura;
+        this.precipitacao = precipitacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Ano: " + ano + ", Mes: " + mes + ", Temp: " + temperatura + ", Chuva: " + precipitacao;
+    }
+    
+}
